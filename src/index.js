84a390a82b1f692db45f4d61b0a0cd579070f3fc
WTech58 <zc949466@gmail.com>
2025-01-31 16:40:32 +0800

    Create index.js && create react template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+       <>
+      {/* 開始web */}
+      </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
