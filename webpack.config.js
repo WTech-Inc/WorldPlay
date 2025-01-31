873a8b635d710f8daff7fa3c11c2f52a33920303
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-01-31 21:57:18 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'src'),
+        path: path.resolve(__dirname, 'op'),
         filename: 'bundle.js',
     },
     module: {
