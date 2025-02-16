4caa131d59bd7158b853acfb45097e2ee3eccda4
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-02-16 18:13:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // 提供靜態文件
-app.use(express.static(path.join(__dirname,'op')));
+app.use(express.static(path.join(__dirname,'src')));
 
 // 返回 html 文件
 app.get('/', (req, res) => {
